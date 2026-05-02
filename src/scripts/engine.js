ba7fda441cccf425cf84ccd8f2c1dc4739d51dcb
lavi <165945697+scriptlver@users.noreply.github.com>
2026-05-01 22:17:47 -0300

    nothing at all - classes dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,17 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+};
+
+pianoKeys.forEach((key) => {
+  console.log(key.dataset.key);
+  key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+document.addEventListener("keydown", (e) => {
+    playTune(e.key);
+});
